f2400b7388b3d151464bdc3c7d2b150199422288
Samperc <santiperezmonge@gmail.com>
2022-09-09 19:05:14 -0300

    punto1

---
+++ LogTest/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using NLog;
+
+Console.WriteLine("Hello, World!");
+Logger Logger =NLog.LogManager.GetCurrentClassLogger();
+try
+{
+  Logger.Info("Hello World");  
+}
+catch (System.Exception)
+{
+    
+    Logger.Error("Goodbay cruel wordl");
+}
+
